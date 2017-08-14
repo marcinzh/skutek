3efcd83550bc9e69f67ebbb0bcc63de076b72ce6
marcinzh <marcinzh1@yahoo.com>
2017-08-15 00:34:41 +0200

    NoChoice and Writer.monoid

--- core/src/main/scala/skutek/Writer.scala
+++ core/src/main/scala/skutek/Writer.scala
@@ -12,13 +12,13 @@ abstract class WriterHandler[T] extends UnaryHandler[Writer[T]] {
   type Result[A] = (A, Stan)
   type Op[A] = WriterOperation[A, T]
 
-  def add(c1: Stan, c2: Stan): Stan
+  def add(s1: Stan, s2: Stan): Stan
   def single(x: T): Stan
 
-  final def onReturn[A](a: A) = c => Return((a, c))
+  final def onReturn[A](a: A) = s => Return((a, s))
 
   final def onOperation[A, B, U](op: Op[A], k: A => Stan => Result[B] !! U): Stan => Result[B] !! U = op match {
-    case Tell(x) => c => k(())(add(c, single(x)))
+    case Tell(x) => s => k(())(add(s, single(x)))
   }
 
   final def onProduct[A, B, C, U](
@@ -26,9 +26,9 @@ abstract class WriterHandler[T] extends UnaryHandler[Writer[T]] {
     y: Stan => Result[B] !! U, 
     k: ((A, B)) => Stan => Result[C] !! U
   ): Stan => Result[C] !! U = 
-    (c: Stan) => 
+    (s: Stan) => 
       (x(initial) *! y(initial)).flatMap { 
-        case ((a, c1), (b, c2)) => k((a, b))(add(add(c, c1), c2)) 
+        case ((a, s1), (b, s2)) => k((a, b))(add(add(s, s1), s2)) 
       }
 
   def exec = new MappedHandler[Lambda[A => Stan]] {
@@ -48,7 +48,14 @@ object WriterHandler {
   def seq[T]() = new WriterHandler[T] {
     type Stan = Vector[T]
     def initial = Vector.empty[T]
-    def add(c1: Stan, c2: Stan) = c1 ++ c2
+    def add(s1: Stan, s2: Stan) = s1 ++ s2
     def single(x: T) = Vector(x)
+  }
+
+  def monoid[T](zero: T, add: (T, T) => T) = new WriterHandler[T] {
+    type Stan = T
+    def initial = zero
+    def add(x: T, y: T) = add(x, y)
+    def single(x: T) = x
   }
 }
