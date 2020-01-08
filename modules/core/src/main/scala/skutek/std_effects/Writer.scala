7178babc4263e95c530e4ded9ebd9f525aae1dd4
marcinzh <marcinzh1@yahoo.com>
2020-01-08 21:39:38 +0100

    faster Writer

--- modules/core/src/main/scala/skutek/std_effects/Writer.scala
+++ modules/core/src/main/scala/skutek/std_effects/Writer.scala
@@ -8,28 +8,20 @@ trait Writer[W] extends EffectImpl {
   case class Tell(value: W) extends Op[Unit]
   def Tell[X](x: X)(implicit ev: SingletonCons[X, W]): Unit !! this.type = Tell(ev.singletonCons(x))
 
-  def handler(implicit W: Monoid[W]) = new Nullary with Parallel {
+  def handler(implicit W: Monoid[W]) = (new Unary[W] with Parallel {
     type Result[A] = (A, W)
 
     def onReturn[A, U](a: A): A !@! U =
-      Return((a, Monoid[W].empty))
+      w => Return((a, w))
 
     def onOperation[A, B, U](op: Op[A], k: A => B !@! U): B !@! U =
       op match {
-        case Tell(w1) =>
-          k(()).map {
-            case (b, w2) => (b, w1 |@| w2)
-          }
+        case Tell(w1) => w0 => k(())(w0 |@| w1)
       }
 
-    def onProduct[A, B, C, U](ma: A !@! U, mb: B !@! U, k: ((A, B)) => C !@! U): C !@! U =
-      (ma *! mb).flatMap {
-        case ((a, w1), (b, w2)) =>
-          val w12 = w1 |@| w2
-          k((a, b)).map {
-            case (c, w3) =>
-              (c, w12 |@| w3)
-          }
+    def onProduct[A, B, C, U](tma: A !@! U, tmb: B !@! U, k: ((A, B)) => C !@! U): C !@! U =
+      w0 => (tma(W.empty) *! tmb(W.empty)).flatMap {
+        case ((a, w1), (b, w2)) => k((a, b))(w0 |@| w1 |@| w2)
       }
-  }
+  }).apply(W.empty)
 }
